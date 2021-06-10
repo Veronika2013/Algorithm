846fb315e40f33c6c096df091db0f1e1134cc75d
JunSun <63651740+Veronika2013@users.noreply.github.com>
2021-06-10 08:39:03 +0300

    Add files via upload

---
+++ selection_sort.cpp
@@ -0,0 +1,40 @@
+﻿
+
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+
+using namespace std;
+
+void selectionSort()
+{
+    int arr [10];
+    for (int i = 0; i < 10; i++) {
+        arr[i] = rand() % 1000;
+    }
+
+    int j = 0;
+    int tmp = 0;
+    for (int i = 0; i < 10; i++) {
+        j = i;
+        for (int k = i; k < 10; k++) {
+            if (arr[j] > arr[k]) {
+                j = k;
+            }
+        }
+        tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+    for (int i = 0; i < 10; i++) {
+        cout << arr[i] << " ";
+    }
+}
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+    
+    selectionSort();
+	return 0;
+    }
